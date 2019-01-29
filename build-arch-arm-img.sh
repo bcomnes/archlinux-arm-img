a9bfba62349122e10005303bea04682f5dcc1e89
Bret Comnes <bcomnes@gmail.com>
2019-01-29 09:51:39 -0800

    Cache downloads

--- build-arch-arm-img.sh
+++ build-arch-arm-img.sh
@@ -3,20 +3,29 @@
 set -e
 set -x
 
+IMG_DIR="images"
 IMG_URL="$1"
 IMG_NAME=${IMG_URL##*/}
+IMG_PATH=${IMG_DIR}/${IMG_NAME}
 TARGET_IMAGE=$(basename -s .tar.gz "$IMG_NAME").img
 TARGET_IMAGE_MD5=${TARGET_IMAGE}.md5
 MD5_URL=${IMG_URL}.md5
 MD5_NAME=${MD5_URL##*/}
 
 losetup /dev/loop0 && exit 1 || true
-archive=$IMG_NAME
-url=$IMG_URL
 
-wget -N $IMG_URL
-wget -N $MD5_URL
-md5sum -c $MD5_NAME
+## Download the md5
+mkdir -p IMG_DIR
+pushd $IMG_DIR
+wget -N -$MD5_URL
+if md5sum -c $MD5_NAME ; then
+  echo "Cached ${IMG_NAME} already downloaded!"
+else
+  echo "Cached ${IMG_NAME} did not match MD5 of latest image, downloading"
+  wget -N $IMG_URL
+  md5sum -c $MD5_NAME
+fi
+popd
 
 truncate -s 1500M $TARGET_IMAGE
 losetup /dev/loop0 $TARGET_IMAGE
@@ -31,7 +40,7 @@ mkfs.ext4 -F -L root -b 4096 -E stride=4,stripe_width=1024 /dev/loop0p2
 mkdir -p root
 mount /dev/loop0p2 root
 
-bsdtar xfz $IMG_NAME -C root
+bsdtar xfz $IMG_PATH -C root
 mv root/boot root/boot-temp
 mkdir -p root/boot
 mount /dev/loop0p1 root/boot
