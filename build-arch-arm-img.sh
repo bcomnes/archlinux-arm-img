0e142625412aa8db1550dfcacb0d6b8b2b43a3fe
Bret Comnes <bcomnes@gmail.com>
2019-01-29 10:31:09 -0800

    Cache lib archive

--- build-arch-arm-img.sh
+++ build-arch-arm-img.sh
@@ -14,7 +14,7 @@ MD5_NAME=${MD5_URL##*/}
 
 losetup /dev/loop0 && exit 1 || true
 
-## Download the md5
+## Check cache and maybe download
 mkdir -p IMG_DIR
 pushd $IMG_DIR
 wget -N $MD5_URL
@@ -23,10 +23,12 @@ if md5sum -c $MD5_NAME ; then
 else
   echo "Cached ${IMG_NAME} did not match MD5 of latest image, downloading"
   wget -N $IMG_URL
+  # Double check the new version matches
   md5sum -c $MD5_NAME
 fi
 popd
 
+# Set up image file
 truncate -s 1500M $TARGET_IMAGE
 losetup /dev/loop0 $TARGET_IMAGE
 parted -s /dev/loop0 mklabel msdos
@@ -37,19 +39,27 @@ parted -s /dev/loop0 print
 mkfs.vfat -I -n SYSTEM /dev/loop0p1
 mkfs.ext4 -F -L root -b 4096 -E stride=4,stripe_width=1024 /dev/loop0p2
 
+# Mount image
 mkdir -p root
 mount /dev/loop0p2 root
 
+# Copy image contents over
 bsdtar xfz $IMG_PATH -C root
 mv root/boot root/boot-temp
 mkdir -p root/boot
 mount /dev/loop0p1 root/boot
 mv root/boot-temp/* root/boot/
 rm -rf root/boot-temp
+
+# Turn off access time?
 sed -i "s/ defaults / defaults,noatime /" root/etc/fstab
+
+# Cleanup
 umount root/boot root
 losetup -d /dev/loop0
 rm -rf root
+
+# Generate MD5
 md5sum $TARGET_IMAGE > $TARGET_IMAGE_MD5
 
 # Take from https://gist.github.com/larsch/4ae5499023a3c5e22552
