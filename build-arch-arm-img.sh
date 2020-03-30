103d4705e71d84c54ee266f6750dfc3ac3d484a2
Bret Comnes <bcomnes@gmail.com>
2020-03-29 21:17:38 -0600

    try different loop path

--- build-arch-arm-img.sh
+++ build-arch-arm-img.sh
@@ -13,7 +13,7 @@ TARGET_ZIP_MD5=${TARGET_ZIP}.md5
 MD5_URL=${IMG_URL}.md5
 MD5_NAME=${MD5_URL##*/}
 
-losetup /dev/loop0 && exit 1 || true
+losetup /dev/loop10 && exit 1 || true
 
 ## Check cache and maybe download
 mkdir -p IMG_DIR
@@ -31,24 +31,24 @@ popd
 
 # Set up image file
 truncate -s 1500M "${TARGET_IMAGE}"
-losetup /dev/loop0 "${TARGET_IMAGE}"
-parted -s /dev/loop0 mklabel msdos
-parted -s /dev/loop0 mkpart primary fat32 -a optimal -- 0% 100MB
-parted -s /dev/loop0 set 1 boot on
-parted -s /dev/loop0 unit mb mkpart primary ext2 -a optimal -- 100MB 100%
-parted -s /dev/loop0 print
-mkfs.vfat -I -n SYSTEM /dev/loop0p1
-mkfs.ext4 -F -L root -b 4096 -E stride=4,stripe_width=1024 /dev/loop0p2
+losetup /dev/loop10 "${TARGET_IMAGE}"
+parted -s /dev/loop10 mklabel msdos
+parted -s /dev/loop10 mkpart primary fat32 -a optimal -- 0% 100MB
+parted -s /dev/loop10 set 1 boot on
+parted -s /dev/loop10 unit mb mkpart primary ext2 -a optimal -- 100MB 100%
+parted -s /dev/loop10 print
+mkfs.vfat -I -n SYSTEM /dev/loop10p1
+mkfs.ext4 -F -L root -b 4096 -E stride=4,stripe_width=1024 /dev/loop10p2
 
 # Mount image
 mkdir -p root
-mount /dev/loop0p2 root
+mount /dev/loop10p2 root
 
 # Copy image contents over
 bsdtar xfz "${IMG_PATH}" -C root
 mv root/boot root/boot-temp
 mkdir -p root/boot
-mount /dev/loop0p1 root/boot
+mount /dev/loop10p1 root/boot
 mv root/boot-temp/* root/boot/
 rm -rf root/boot-temp
 
@@ -57,7 +57,7 @@ sed -i "s/ defaults / defaults,noatime /" root/etc/fstab
 
 # Cleanup
 umount root/boot root
-losetup -d /dev/loop0
+losetup -d /dev/loop10
 rm -rf root
 
 # Zip img
